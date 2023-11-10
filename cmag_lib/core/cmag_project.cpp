9010e1fe438d1043d790ef20bad065a91f8f0c8b
MaciejDziuban <dziuban.maciej@gmail.com>
2023-11-10 01:43:00 +0100

    Add fixupLinkLibraries

--- cmag_lib/core/cmag_project.cpp
+++ cmag_lib/core/cmag_project.cpp
@@ -1,6 +1,7 @@
 #include "cmag_project.h"
 
 #include <algorithm>
+#include <cstring>
 
 void CmagProject::addTargetGraphical(std::string_view targetName, float x, float y) {
     auto targetIt = std::find_if(targets.begin(), targets.end(), [targetName](const CmagTarget &target) {
@@ -58,6 +59,101 @@ bool CmagProject::mergeTargets(CmagTarget &dst, CmagTarget &&src) {
 void CmagProject::addConfig(std::string_view config) {
     if (std::find(configs.begin(), configs.end(), config) == configs.end()) {
         configs.push_back(std::string(config));
+    }
+}
+
+void CmagTargetConfig::fixupLinkLibraries(std::string_view propertyName, std::string_view nonEvaledValue) {
+    // LINK_LIBRARIES and INTERFACE_LINK_LIBRARIES may contain a generator expression $<LINK_ONLY:XXX>.
+    // This resolves to XXX, when evaluating with $<GENEX_EVAL>, even though we would expect it
+    // to be empty. As a workaround we dump both evaled and non-evaled versions. Non evaled version can
+    // to look up which entries are in form $<LINK_ONLY:XXX>. Then we can remove these entries from
+    // evaled version.
+
+    // Find the property
+    auto it = std::find_if(properties.begin(), properties.end(), [propertyName](const CmagTargetProperty &p) {
+        return p.name == propertyName;
+    });
+    if (it == properties.end()) {
+        // Technically this should never happen and we should crash, but let's be liberal and ignore this.
+        return;
+    }
+    std::string &evaledValue = it->value;
+
+    // Early return if we have no genexes
+    if (nonEvaledValue.find('$') == std::string::npos) {
+        return;
+    }
+
+    // Find all values wrapped with $<LINK_ONLY>
+    const char *prefix = "$<LINK_ONLY:";
+    const size_t prefixLength = strlen(prefix);
+    size_t genexDepth = 0;
+    size_t elementIndex = 0;
+    bool isPrefixMatched = false;
+    std::vector<size_t> elementIndicesToRemove = {};
+    for (size_t positionInNonEvaled = 0; positionInNonEvaled < nonEvaledValue.length(); positionInNonEvaled++) {
+        const char currentChar = nonEvaledValue[positionInNonEvaled];
+        const char previousChar = positionInNonEvaled > 0 ? nonEvaledValue[positionInNonEvaled - 1] : '\0';
+
+        // Update regex depth
+        if (previousChar == '$' && currentChar == '<') {
+            genexDepth++;
+            continue;
+        }
+        if (previousChar != '\\' && currentChar == '>') {
+            if (genexDepth == 0) {
+                // Malformed genex, don't try to save it
+                return;
+            }
+
+            genexDepth--;
+            if (genexDepth == 0 && isPrefixMatched) {
+                elementIndicesToRemove.push_back(elementIndex);
+                isPrefixMatched = false;
+            }
+            continue;
+        }
+
+        // Update element index
+        if (previousChar != '\\' && currentChar == ';' && genexDepth == 0) {
+            elementIndex++;
+            continue;
+        }
+
+        // Check if we're inside our cursed genex
+        if (positionInNonEvaled >= prefixLength && genexDepth == 1 && !isPrefixMatched) {
+            std::string_view currentPrefix = nonEvaledValue.substr(positionInNonEvaled - prefixLength, prefixLength);
+            if (prefix == currentPrefix) {
+                isPrefixMatched = true;
+            }
+        }
+    }
+
+    // Remove all entries with detected $<LINK_ONLY> genex by their index
+    elementIndex = 0;
+    size_t elementStartPosition = 0;
+    for (size_t positionInEvaled = 0; positionInEvaled < evaledValue.length() + 1; positionInEvaled++) {
+        const char currentChar = evaledValue.data()[positionInEvaled];
+        if (currentChar != ';' && currentChar != '\0') {
+            continue;
+        }
+
+        if (std::find(elementIndicesToRemove.begin(), elementIndicesToRemove.end(), elementIndex) != elementIndicesToRemove.end()) {
+            std::size_t removeStart = elementStartPosition;
+            std::size_t removeEnd = positionInEvaled;
+            if (removeStart > 0 && evaledValue[removeStart - 1] == ';') {
+                removeStart--;
+            } else if (removeEnd < evaledValue.length() - 1 && evaledValue[removeEnd] == ';') {
+                removeEnd++;
+            }
+            evaledValue.erase(removeStart, removeEnd - removeStart);
+
+            positionInEvaled = elementStartPosition;
+        } else {
+            elementStartPosition = positionInEvaled + 1;
+        }
+
+        elementIndex++;
     }
 }
 
