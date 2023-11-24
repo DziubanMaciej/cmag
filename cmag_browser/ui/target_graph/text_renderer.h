01dbb44f16c22bdca6eed0829130f8e1f0fe7d5d
MaciejDziuban <dziuban.maciej@gmail.com>
2023-11-24 18:46:21 +0100

    Refactor TextRenderer, so we compile program only once

--- cmag_browser/ui/target_graph/text_renderer.h
+++ cmag_browser/ui/target_graph/text_renderer.h
@@ -12,25 +12,39 @@ struct ImFont;
 
 class TextRenderer {
 public:
-    TextRenderer(std::string_view text);
+    TextRenderer();
     TextRenderer(TextRenderer &&other) = default;
     TextRenderer &operator=(TextRenderer &&other) = default;
     ~TextRenderer();
 
-    void render(glm::mat4 transform);
+    void render(glm::mat4 transform, std::string_view text, ImFont *font);
 
 private:
-    void allocateVertexBuffer(ImFont *font, std::string_view text);
     void allocateProgram();
 
-    static std::vector<float> prepareVertexData(ImFont *font, std::string_view text);
+    struct PerStringData {
+        PerStringData(ImFont *font, std::string_view text);
+        PerStringData(PerStringData &&other) = default;
+        PerStringData &operator=(PerStringData &&other) = default;
+        ~PerStringData();
 
-    MovablePrimitive<GLuint> vertexCount;
+        void allocateVertexBuffer(ImFont *font, std::string_view text);
+        static std::vector<float> prepareVertexData(ImFont *font, std::string_view text);
+
+        std::string string;
+        struct {
+            MovablePrimitive<GLuint> vbo;
+            MovablePrimitive<GLuint> vao;
+        } gl;
+        MovablePrimitive<GLuint> vertexCount;
+    };
+
+    PerStringData &getStringData(std::string_view text, ImFont *font);
+    GLuint getTextureId(ImFont *font);
+
+    std::vector<PerStringData> strings;
+
     struct {
-        MovablePrimitive<GLuint> texture;
-        MovablePrimitive<GLuint> vbo;
-        MovablePrimitive<GLuint> vao;
-
         MovablePrimitive<GLuint> program;
         struct {
             MovablePrimitive<GLint> transform;
