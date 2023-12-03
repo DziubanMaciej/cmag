57b8a8fcdb14f3d6b95fea0a1664c82a15f62d7d
MaciejDziuban <dziuban.maciej@gmail.com>
2023-12-03 22:25:51 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,8 @@ if [ -z "$build_dir" ]; then
 fi
 
 pushd "$build_dir"
-cmake --build . --target cmag cmag-browser || exit 1
+cmake --build . --target cmag cmag_browser || exit 1
 popd
 
-bin_dir="$build_dir/bin"
-"$bin_dir"/cmag -p self_run cmake -S . -B "$build_dir" || exit 1
-"$bin_dir"/cmag-browser "$build_dir"/self_run.cmag-project || exit 1
+bin_dir="$build_dir/bin/Debug"
+"$bin_dir"/cmag -p self_run -g cmake -S . -B "$build_dir" || exit 1
