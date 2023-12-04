743d5cc67d96bbffc2c109a180d7d6c5d94a6b89
MaciejDziuban <dziuban.maciej@gmail.com>
2023-12-04 02:05:29 +0100

    Add tooltip for config selector

--- cmag_browser/ui/config_selector.cpp
+++ cmag_browser/ui/config_selector.cpp
@@ -19,7 +19,22 @@ void ConfigSelector::render(float width) {
     if (width != 0) {
         ImGui::SetNextItemWidth(width);
     }
+    if (selectionsCount == 1) {
+        ImGui::BeginDisabled();
+    }
     ImGui::Combo("##ConfigSelection", &currentSelection, configs.get(), selectionsCount);
+    if (selectionsCount == 1) {
+        ImGui::EndDisabled();
+
+        // TODO wrap tooltips in some shared helper
+        if (ImGui::BeginItemTooltip()) {
+            const int wrapWidth = 300;
+            ImGui::PushTextWrapPos(ImGui::GetCursorPos().x + wrapWidth);
+            ImGui::Text("Only %s config is available. This is common for single config generators. See output of cmag -h about --merge option to be able to compare multiple configs", configs[0]);
+            ImGui::PopTextWrapPos();
+            ImGui::EndTooltip();
+        }
+    }
 }
 
 std::string_view ConfigSelector::getCurrentConfig() {
